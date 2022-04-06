2be716c2b1d8141555fb08e9950db6ea7dcb7f27
Julita Inca <jinca@pucp.pe>
2022-04-06 16:39:14 +0100

    Creating files using lists and dictionaries

--- create_files_test2.py
+++ create_files_test2.py
@@ -1,9 +1,42 @@
+import codecs
 import sys
-import os
-import numpy as np
-import pandas as pd
 
-l = []
-with open("/home/yulytas/python_training/test2.txt") as f:
-    l = [line.strip() for line in f  if "\t" not in line]
+with open("test2.txt","r") as f:
+    indented_text = f.read()
+    
+class Node:
+    def __init__(self, indented_line):
+        self.children = []
+        self.level = len(indented_line) - len(indented_line.lstrip())
+        self.text = indented_line.strip()
 
+    def add_children(self, nodes):
+        childlevel = nodes[0].level
+        while nodes:
+            node = nodes.pop(0)
+
+            if node.level == childlevel: # add node as a child
+                self.children.append(node)
+            elif node.level > childlevel: # add nodes as grandchildren of the last child
+                nodes.insert(0,node)
+                self.children[-1].add_children(nodes)
+            elif node.level <= self.level: # this node is a sibling, no more children
+                nodes.insert(0,node)
+                return
+
+    def as_dict(self):
+        if len(self.children) > 1:
+                return {self.text: [node.as_dict() for node in self.children]}
+        elif len(self.children) == 1:
+                return {self.text: self.children[0].as_dict()}
+        else:
+                return self.text
+        
+root = Node('root')
+root.add_children([Node(line) for line in indented_text.splitlines() if line.strip()])
+d = root.as_dict()['root']
+
+for i in d:
+    for x,y in i.items():
+        for a in y:
+            print(x,a)
